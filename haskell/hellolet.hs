b704ba7fceb851871bb24afe07b46d34776fcc22
Keith Mannock <keith@dcs.bbk.ac.uk>
2020-03-18 10:19:39 +0000

    Corrected typo

--- haskell/hellolet.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Data.Char  
-  
-main = do  
-    putStrLn "What's your name?"  
-    name <- getLine  
-    let bigName = map toUpper name  
-    putStrLn ("Hi " ++ bigName ++ ", how are you?")  
