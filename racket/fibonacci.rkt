f7ae7c3557b4281f72984d7ff9a8347973f9dddf
Trevor Fenner <32911321+trevorfenner@users.noreply.github.com>
2020-11-12 01:27:11 +0000

    Update fibonacci.rkt

--- racket/fibonacci.rkt
+++ racket/fibonacci.rkt
@@ -5,7 +5,24 @@
       1
       (+ (fibonacci (- x 1))
          (fibonacci (- x 2)))))
-
+         
+(define (fibonacciTR x)
+  (define (f acc1 acc2 y)
+    (if (= y x)
+        (+ acc1 acc2)
+        (f (+ acc1 acc2) acc1 (+ y 1))))
+  (if (or (= x 1) (= x 2))
+      1
+      (f 1 1 3)))
+      
+;;;improved and more transparent tail-recursive version
+(define (fibonacciTR2 n)
+  (define (f acc1 acc2 n)
+    (cond ((= 1 n) acc1)
+          ( (= 2 n) acc2)
+          (else (f acc2 (+ acc1 acc2) (- n 1)))))
+  (f 1 1 n))
+  
 ;(define (fibonacci x)
 ;  (letrec ([f (lambda (acc1 acc2 y)
 ;                (if (= y x)
