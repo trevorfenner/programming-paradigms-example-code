b45b0d56d3627520c7e5ec47cf42f31524f0ab0c
Trevor Fenner <32911321+trevorfenner@users.noreply.github.com>
2020-11-12 01:28:24 +0000

    Update tailrecreverse.rkt

--- racket/tailrecreverse.rkt
+++ racket/tailrecreverse.rkt
@@ -19,9 +19,6 @@
       (cons n (revnums (- n 1)))))
 ;;;(revnums n) is the same as (range n 0 -1)
 
-;;;(take x k) is a list of the first k elements of the list x
-(take (revnums 30000) 8)
-
 ;;;the following calls show the difference using tail recursion
 (take (TRreverse (revnums 30000)) 8)
 (take (reverse (revnums 30000)) 8)
