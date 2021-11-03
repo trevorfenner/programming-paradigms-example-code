1471c961e0f75be16673d96c1d203f8444bfc8fb
Trevor Fenner <32911321+trevorfenner@users.noreply.github.com>
2021-11-03 17:49:56 +0000

    Update 04-fibonacci.rkt

--- racket/04-fibonacci.rkt
+++ racket/04-fibonacci.rkt
@@ -15,6 +15,15 @@
       1
       (f 1 1 3)))
       
+;(define (fibonacciTR x)
+;  (letrec ([f (lambda (acc1 acc2 y)
+;                (if (= y x)
+;                    (+ acc1 acc2)
+;                    (f (+ acc1 acc2) acc1 (+ y 1))))])
+;    (if (or (= x 1) (= x 2))
+;        1
+;        (f 1 1 3))))      
+      
 ;;;improved and more transparent tail-recursive version
 (define (fibonacciTR2 n)
   (define (f acc1 acc2 n)
@@ -23,15 +32,9 @@
           (else (f acc2 (+ acc1 acc2) (- n 1)))))
   (f 1 1 n))
   
-;(define (fibonacci x)
-;  (letrec ([f (lambda (acc1 acc2 y)
-;                (if (= y x)
-;                    (+ acc1 acc2)
-;                    (f (+ acc1 acc2) acc1 (+ y 1))))])
-;    (if (or (= x 1) (= x 2))
-;        1
-;        (f 1 1 3))))
+
 ;
+;;;   Memoised version of fibonacci
 ;(define fibonacci
 ;  (letrec([memo null]
 ;          [f (lambda (x)
